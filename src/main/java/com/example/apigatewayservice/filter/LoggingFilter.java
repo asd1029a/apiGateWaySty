d4c41b536ef3da804f821d0c654e0609c057b8c0
김정민 <kimjm9347@danusys.com>
2023-05-10 16:52:23 +0900

    sty 5

--- src/main/java/com/example/apigatewayservice/filter/LoggingFilter.java
+++ src/main/java/com/example/apigatewayservice/filter/LoggingFilter.java
@@ -52,7 +52,7 @@ public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Co
             log.info("Logging Filter baseMessage: {}", config.getBaseMessage());
 
             if (config.isPreLogger()) {
-                log.info("Logging Filter Start: request id -> {}", request.getId());
+                log.info("Logging Filter Start:: request id -> {}", request.getId());
             }
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 if (config.isPostLogger()) {
