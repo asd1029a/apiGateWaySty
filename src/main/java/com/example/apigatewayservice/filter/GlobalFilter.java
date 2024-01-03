44d76c1934a4d174e67ba3db5c446309c7334b50
kjm <kimth5533@naver.com>
2024-01-03 14:38:41 +0900

    잔디잔디..

--- src/main/java/com/example/apigatewayservice/filter/GlobalFilter.java
+++ src/main/java/com/example/apigatewayservice/filter/GlobalFilter.java
@@ -35,7 +35,7 @@ public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Conf
 
             if (config.isPreLogger()) {
                 log.info("Global Filter Start: request id -> {}", request.getId());
-                log.info("Test log OK? ");
+                log.info("Test log OK? nono ");
             }
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 if (config.isPostLogger()) {
